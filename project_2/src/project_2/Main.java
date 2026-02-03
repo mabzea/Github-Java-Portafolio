f8460a7d300e883e4822a2bbe96eac4fdc40a615
mabzea <56006619+mabzea@users.noreply.github.com>
2026-02-03 17:23:19 -0600

    Process Changes

--- project_2/src/project_2/Main.java
+++ project_2/src/project_2/Main.java
@@ -1,5 +1,17 @@
 package project_2;
 
 public class Main {
+	
+  public class helloWorld {
+	  
+	  public static void main (String[] args) {
+		  
+		  System.out.println ("Hello World!");
+		  
+	  }
+			  
+  }
+  
+  
 
 }
