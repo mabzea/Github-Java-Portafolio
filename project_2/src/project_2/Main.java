3efb1e690bb447d08ed3d1ecf2138005b6c25118
mabzea <56006619+mabzea@users.noreply.github.com>
2026-02-03 17:24:19 -0600

    Revert Changes

--- project_2/src/project_2/Main.java
+++ project_2/src/project_2/Main.java
@@ -2,15 +2,6 @@ package project_2;
 
 public class Main {
 	
-  public class helloWorld {
-	  
-	  public static void main (String[] args) {
-		  
-		  System.out.println ("Hello World!");
-		  
-	  }
-			  
-  }
   
   
 
